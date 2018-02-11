9b8c38448582c3ea718430a551912e394489fefb
daviddalbusco <david.dalbusco@outlook.com>
2018-02-11 11:57:26 +0100

    Add camera photo

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import {Camera, CameraOptions, CameraPopoverOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,53 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  imgURI: string;
 
+  constructor(private camera: Camera) {
+
+  }
+
+  open() {
+    this.getPicture(this.camera.PictureSourceType.CAMERA);
+  }
+
+  private getPicture(sourceType: number) {
+
+    //noinspection TypeScriptUnresolvedVariable
+    let popoverOptions: CameraPopoverOptions = {
+      x: 0,
+      y: 0,
+      width: 1080,
+      height: 1080,
+      arrowDir: this.camera.PopoverArrowDirection.ARROW_ANY
+    };
+
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: sourceType,
+      allowEdit: false,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: false,
+      cameraDirection: this.camera.Direction.BACK,
+      popoverOptions: popoverOptions
+    };
+
+    this.camera.getPicture(options).then((imageURI: string) => {
+      this.imgURI = this.wkWebViewFileURI(imageURI);
+    }, (err: string) => {
+      console.error(err);
+    });
+  }
+
+  private wkWebViewFileURI(uri: string): string {
+    if (uri !== null && uri.indexOf('file://') > -1) {
+      uri = uri.replace('file://', '');
+    }
+
+    return uri;
   }
 
 }
